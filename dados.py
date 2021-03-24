6378f6ed8b45b2dc7d52536cf61d379db5438c60
Rafael Agra <36060418+rafaelmspu2@users.noreply.github.com>
2021-03-24 12:33:07 -0300

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from matplotlib import pyplot as plt
+import pandas as pd
+from urllib.request import urlopen
+
+def get_data_bcb(código):
+    url = "http://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados?formato=json".format(código)
+    html = urlopen(url).read()
+    df = pd.read_json(html)
+    df.index = pd.to_datetime(df['data'])
+    df = df['valor']
+    return df
+
+selic = get_data_bcb(11)
+
+plt.plot(selic.index, selic)
+plt.xlabel('Anos')
+plt.ylabel('Taxa')
+plt.title('Taxa Selic')
+plt.show()
